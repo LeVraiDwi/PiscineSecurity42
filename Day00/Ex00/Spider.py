0087730df05a9614bbef68ad23fee6c9c67a0a06
Theo COSSE <tcosse@paul-f4Ar1s1.clusters.42paris.fr>
2024-10-24 17:58:33 +0200

    soup

--- Day00/Ex00/Spider.py
+++ Day00/Ex00/Spider.py
@@ -1,36 +1,64 @@
-from urllib.request import urlopen
-import re
-import string
+from urllib.request import urlopen, Request
+import requests
+import shutil
+from bs4 import BeautifulSoup
 
 def baseDomaine(url: str) -> str:
     protocol : str
     try:
-        assert url[:8] is "https://" or url[:7] is "http://", "url wrong format"
+        assert url.__len__() >= 7
+        protocol = ""
+        if url[:8] == "https://":
+            protocol = url[:8]
+        elif url[:7] == "http://":
+            protocol = url[:7]
+        assert protocol  != "", "url wrong format"
+        assert protocol.__len__() < url.__len__(), "Url wrong format"
     except AssertionError:
         print(AssertionError)
-        return
-        
-    return sds
+        return ""
+    endDomain = url[protocol.__len__():].find("/")
+    domain = url[: endDomain + protocol.__len__()]
+    return domain
 
-
-def main():
+def ScrapImg(Url: str, depth: int):
     url = "http://olympus.realpython.org/profiles/aphrodite"
-    page = urlopen(url)
-    page
+    req = Request(
+        url=url, 
+        headers={'User-Agent': 'Mozilla/5.0'}
+    )
+    imgExt = [".jpg", ".jpeg", ".png", ".gif",".bmp"]
+    domain = baseDomaine(url)
+    if domain == "":
+        print("wrong url")
+        return
+    page: _UrlopenRet
+    try:
+        page = urlopen(req)
+    except:
+        print("Impossible to Open the page")
+        return
     html_bytes = page.read()
     html = html_bytes.decode("utf-8")
-    print(html)
-    startIndex = [m.start() for m in re.finditer("<img", html)]
-    for start in startIndex:
-        print(html[start:])
-        end = (html[start:].find("/>"))
-        balise = html[start:(start + end + 2)]
-        print(balise)
-        srcStart = balise.find("\"") + 1
-        srcEnd = balise[srcStart:].find("\"")
-        src = balise[srcStart:(srcStart + srcEnd)]
-        print(src)
+    extract = BeautifulSoup(html, 'html.parser')
+    for start in extract.find_all('img'):
+        try:
+            out = start.get('src')
+            if (out and out.endswith(ext) for ext in imgExt):
+                res = requests.get(domain + out, stream=True)
+                if res.status_code == 200:
+                    with open("./name",'wb') as f:
+                        shutil.copyfileobj(res.raw, f)
+                    print('Image sucessfully Downloaded: ',"./name")
+                else:
+                    print('Image Couldn\'t be retrieved')
+        except:
+            print("fail to save the image")
+            continue
     return
+
+def main():
+    
 
 if __name__ == "__main__":
     main()
